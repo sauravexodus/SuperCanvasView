ebb76900f41ee43af7281e463e5312e44d04853e
Sourav Chandra <saurav.chandra1992@live.com>
2018-10-12 13:04:13 +0530

    Refactored and rearranged a lot of code

---
+++ SuperCanvasView/ASAwareTableNode/ASAwareTableNode+rx.swift
@@ -0,0 +1,23 @@
+//
+//  ASAwareTableNode+rx.swift
+//  SuperCanvasView
+//
+//  Created by Sourav Chandra on 12/10/18.
+//  Copyright © 2018 Sourav Chandra. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+// MARK: Reactive Extensions
+
+extension Reactive where Base: ASAwareTableNode {
+    var updatesEnded: ControlProperty<Void> {
+        return ControlProperty(values: base.endUpdateSubject, valueSink: base.endUpdateSubject)
+    }
+    
+    var linesUpdated: ControlProperty<LinesWithIndexPath> {
+        return ControlProperty(values: base.linesUpdateSubject, valueSink: base.linesUpdateSubject)
+    }
+}
